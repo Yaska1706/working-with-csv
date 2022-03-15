d4cd68b354444d0f9b0398c30d1a0c17461883cf
Yasin Kinyua Murithi <yasinkinyua@gmail.com>
2022-03-15 16:08:31 +0300

    Change: Add checks
    Signed-off-by: Yasin Kinyua Murithi <yasinkinyua@gmail.com>

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
-	"os"
-)
 
-type employee struct {
-	Name string `json:"name"`
-	Age  string `json:"age"`
-	City string `json:"city"`
-}
+	"github.com/yaska1706/working-with-csv/pkg"
+)
 
 func main() {
 	filename := "employee1.csv"
@@ -24,12 +16,13 @@ func main() {
 		{"William", "34", "Nakuru"},
 		{"Rose", "41", "Mombasa"},
 	}
-	b, err := checkExist(filename)
+	b, err := pkg.CheckExist(filename)
 	if err != nil {
 		fmt.Println("failed check", err)
 	}
 	if b {
-		employeesdata := getDataFromCSV(filename)
+		csvdata := pkg.GetDataFromCSV(filename)
+		employeesdata := pkg.StoreCSVData(csvdata)
 		jsonData, err := json.Marshal(employeesdata)
 		if err != nil {
 			fmt.Println("somithing happened,", err)
@@ -38,93 +31,6 @@ func main() {
 		fmt.Println(string(jsonData))
 	} else {
 
-		writeToCSV(filename, data)
-	}
-}
-
-func getDataFromCSV(filename string) []employee {
-	csvFile, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-
-	fmt.Println("CSV file opened")
-	defer csvFile.Close()
-
-	csvLines, err := csv.NewReader(csvFile).ReadAll()
-	if err != nil {
-		fmt.Println("failed to read csv", err)
-		return nil
-	}
-	employees := make([]employee, 0)
-
-	for i, line := range csvLines {
-		if i > 0 {
-			var emp employee
-			for j, field := range line {
-				if j == 0 {
-					emp.Name = field
-				} else if j == 1 {
-					emp.Age = field
-				} else if j == 2 {
-					emp.City = field
-				}
-			}
-			employees = append(employees, emp)
-		}
-
-	}
-	return employees
-}
-
-func writeToCSV(filename string, data [][]string) {
-	csvFile, err := os.Create(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer csvFile.Close()
-
-	csvWriter := csv.NewWriter(csvFile)
-	for _, row := range data {
-		if err := csvWriter.Write(row); err != nil {
-			fmt.Println("something happened:", err)
-		}
-	}
-	csvWriter.Flush()
-
-}
-
-func WriteAlltoCSV(filename string, data [][]string) {
-	csvFile, err := os.Create(filename)
-	if err != nil {
-		log.Fatal(err)
-
-	}
-
-	csvFile.Close()
-
-	cW := csv.NewWriter(csvFile)
-	if err := cW.WriteAll(data); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func checkExist(filename string) (bool, error) {
-	_, err := os.Stat(filename)
-	if err == nil {
-		return true, nil
-	}
-
-	if errors.Is(err, os.ErrNotExist) {
-		return false, nil
-	}
-
-	return false, err
-}
-
-func DeleteFile(filename string) {
-	if err := os.Remove(filename); err != nil {
-		fmt.Println("failed to delete file:", err)
+		pkg.WriteToCSV(filename, data)
 	}
 }
