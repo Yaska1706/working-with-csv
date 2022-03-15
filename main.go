67243af7b5ca375bc407ac96e4ac0d641a2598cd
Yasin Kinyua Murithi <yasinkinyua@gmail.com>
2022-03-15 16:16:09 +0300

    Change: Refactor
    Signed-off-by: Yasin Kinyua Murithi <yasinkinyua@gmail.com>

--- main.go
+++ main.go
@@ -17,20 +17,30 @@ func main() {
 		{"Rose", "41", "Mombasa"},
 	}
 	b, err := pkg.CheckExist(filename)
-	if err != nil {
-		fmt.Println("failed check", err)
-	}
+	errCheck(err)
 	if b {
-		csvdata := pkg.GetDataFromCSV(filename)
-		employeesdata := pkg.StoreCSVData(csvdata)
-		jsonData, err := json.Marshal(employeesdata)
-		if err != nil {
-			fmt.Println("somithing happened,", err)
-
-		}
-		fmt.Println(string(jsonData))
+		jsonResult := convertCSVtoJSON(filename)
+		fmt.Print(jsonResult)
 	} else {
 
 		pkg.WriteToCSV(filename, data)
+	}
+}
+
+func convertCSVtoJSON(filename string) string {
+
+	csvdata := pkg.GetDataFromCSV(filename)
+	employeesdata := pkg.StoreCSVData(csvdata)
+	jsonData, err := json.Marshal(employeesdata)
+	errCheck(err)
+
+	return string(jsonData)
+
+}
+
+func errCheck(err error) {
+	if err != nil {
+		fmt.Println("somithing happened,", err)
+
 	}
 }
